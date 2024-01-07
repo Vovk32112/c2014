cd8662364aad471d07479d48490d5ff5d2a1b38d
artur <artur.itstep2011@gmail.com>
2024-01-07 15:14:29 +0200

    initial commit

---
+++ Leson4/main.py
@@ -0,0 +1,25 @@
+
+class PurchasePcError(Exception):
+    def __str__(self):
+        return f"Цієї кількості грошей не достатньо для покупки пк!"
+def check_money(amount_of_money, limit_Vaule):
+    if amount_of_money>limit_Vaule:
+        return " Грошей достатньо"
+    else:
+        raise PurchasePcError(amount_of_money)
+money=10
+check_money(money, 500)
+
+
+
+
+class PurchaseMonitorError(Exception):
+    def __str__(self):
+        return f"Цієї кількості грошей не достатньо для покупки монітора!"
+def check_money(amount_of_money, limit_Vaule):
+    if amount_of_money>limit_Vaule:
+        return " Грошей достатньо"
+    else:
+        raise PurchaseMonitorError(amount_of_money)
+money=200
+check_money(money, 150)
