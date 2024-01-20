02178213258891109a60e1929c6b3b46da17cf82
artur <artur.itstep2011@gmail.com>
2024-01-20 15:33:24 +0200

    initial commit

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -4,10 +4,9 @@ respons=requests.get("https://finance.i.ua/")
 respons_text=respons.text
 respons_parse=respons_text.split("<span>")
 for parse_elem_1 in respons_parse:
-    if parse_elem_1.startswith("$"):
+    if parse_elem_1.startswith("USD"):
         for parse_elem_2 in parse_elem_1.split("</span>"):
-            if parse_elem_2.startswith("$") and parse_elem_2[1].isdigit():
+            if parse_elem_2.startswith("USD") and parse_elem_2[1].isdigit():
                 pars_res_list.append(parse_elem_2)
 
-USD=pars_res_list[0]
-print(f"USD - {USD}")
+print(f"USD - {parse_elem_1}")
